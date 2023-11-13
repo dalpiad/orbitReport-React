986c7f84566ecbdab29cd1823a82f0a4a0310293
Doyle Brown <dalpiad@gmail.com>
2023-11-12 22:49:21 -0600

    stuck on the buttons step

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,11 +1,25 @@
 import classes from './styling.css'
+import satData from "./satData.jsx";
+// import filterByType from "../App.jsx";
+// import setSat from "../App.jsx";
+// import displaySats from "../App.jsx";
 
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div className='flex-container'>
+    <div>
       <button>Placeholder Button</button>
       <button>All Orbits</button>
     </div>
+
+    // <div className='flex-container'>
+    //   {displaySats.map((sat, id) => {
+    //     return (
+    //       <button onClick={() => filterByType(sat)} key={id}>
+    //         {sat} Orbit
+    //       </button>); })}
+    //   <button onClick={() => filterByType(setSat)} key={id}>
+    //         {setSat} Orbit</button>
+    // </div>
   );
 };
 
